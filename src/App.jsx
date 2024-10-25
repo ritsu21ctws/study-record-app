c1f5f36807377c83c34263ab7a929e8750ea992f
ritsu <ritsu.21.ctws@gmail.com>
2024-10-25 13:05:16 +0900

    テストデータを一覧で表示

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,21 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const records = [
+    { title: "勉強の記録1", time: 1},
+    { title: "勉強の記録2", time: 3},
+    { title: "勉強の記録3", time: 5}
+  ];
+
   return <>
     <h1>学習記録一覧</h1>
+    <ul>
+      {records.map((record) => (
+        <li key={record.title}>
+          {`${record.title} ${record.time}時間`}
+        </li>
+      ) )}
+    </ul>
   </>;
 }
 
